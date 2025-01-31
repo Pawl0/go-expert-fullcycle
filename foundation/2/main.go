66893e85f18c71ea30ac9480ab79c03724964182
Paulo Henrique Oliveira dos Santos <paulo.hosantos@einstein.br>
2025-01-30 22:22:58 -0300

    first commit

---
+++ foundation/2/main.go
@@ -0,0 +1,20 @@
+package main
+
+const a = "hello world"
+
+var (
+	b  bool = true
+    c int = 10
+	d string = "Wesley"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X"  // string
+	a = "fd"
+	println(a)
+}
+
+func x() {
+
+}
